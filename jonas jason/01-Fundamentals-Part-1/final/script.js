f74cc7ccf595f061bae846027cc8d675a431e529
roberto mongelos <ramgelos1@gmail.com>
2024-01-11 02:39:05 -0300

    estudios developer

---
+++ jonas jason/01-Fundamentals-Part-1/final/script.js
@@ -0,0 +1,28 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaSript is FUN!")
+console.log(40 + 8 + 23 - 10);
+console.log("jonas");
+console.log(23);
+
+let firstName = "Roberto"
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myfirsJob = "programmer"
+console.log(myfirsJob)
+
+let country = "Portugal"
+let continent = "Europa"
+let population = 1000000
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const now = 2037
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+
+console.log(now - 1991 > now - 2018); 
